873124519da0c9cd239350cef0b66ea00c49e382
Chenwy10 <cwy960424@gmail.com>
2024-12-19 13:29:27 -0500

    first commit

--- ultralytics-github/ultralytics/models/yolo/__init__.py
+++
@@ -1,7 +0,0 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from ultralytics.models.yolo import classify, detect, obb, pose, segment, world, parsingpartmatch
-
-from .model import YOLO, YOLOWorld, YOLOPart, SpermParsingPart
-
-__all__ = "classify", "segment", "detect", "pose", "obb", "world", "YOLO", "YOLOWorld", "YOLOPart", "parsingpartmatch"

5d3a87686f78a72c983b8e25023e3c6f68383427
Chenwy10 <cwy960424@gmail.com>
2025-04-16 21:03:31 -0400

    test

--- ultralytics-github/ultralytics/models/yolo/parsingpartWBC/__init__.py
+++
@@ -1,7 +0,0 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from .predict import ParsingPartWBCPredictor
-from .train import ParsingPartWBCTrainer
-from .val import ParsingPartWBCValidator
-
-__all__ = "ParsingPartWBCPredictor", "ParsingPartWBCTrainer", "ParsingPartWBCValidator"
--- ultralytics-github/ultralytics/models/yolo/parsingpartmatch/__init__.py
+++
@@ -1,7 +0,0 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from .predict import ParsingPartMatchPredictor
-from .train import ParsingPartMatchTrainer
-from .val import ParsingPartMatchValidator
-
-__all__ = "ParsingPartMatchPredictor", "ParsingPartMatchTrainer", "ParsingPartMatchValidator"
